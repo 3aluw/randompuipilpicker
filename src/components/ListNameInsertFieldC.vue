325ee1ba7fc00e3b5b10995cccf66eade6ddbaf6
Abdou <2000abdallah2014@gmail.com>
2022-08-08 20:09:29 +0100

    added views file

---
+++ src/components/ListNameInsertFieldC.vue
@@ -0,0 +1,47 @@
+<template>
+ <div class="container">
+<input type="text" v-model="NewlistName"> <br>
+<button @click="ArrayCreator"> Create a new array </button>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'ListNameInsertFieldC',
+data(){
+  return{
+NewlistName : ""
+
+  }
+},
+methods:{
+  ArrayCreator(){
+    this.$emit("NewArrCreator", this.NewlistName);
+    this.NewlistName = '';
+  }
+}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+button{
+  min-width: 20vw;
+  min-height: 4vh;
+  
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,141 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <ListNameInsertFieldC @NewArrCreator="newarrcreator"/>
+  <div class="lists-container">
+<div class="saved-list" v-for="(list, index) in lists" :key="list.number" @click="listExtrcat(index)"> 
+ {{list.number}}
+</div></div>
+<button @click="studentSelector">Pick someone</button>
+<button @click="saver">Save List</button>
+<h1>{{pickedP}}</h1>
+<h2>reamaining students : {{currentObj.arr.length}}</h2>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ListNameInsertFieldC from '../components/ListNameInsertFieldC.vue'
 
 export default {
-  name: 'Home',
+  name: 'App',
   components: {
-    HelloWorld
-  }
+    ListNameInsertFieldC
+  },
+
+  data(){
+    return{
+        arr:[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+    
+      names:["تجرونة بشرى ","باباوموسى هدى","التومي مارية","بوحديبه غنية","موسى وعلي باية","بابنات مروة","شقبقب فاطمة الزهراء","أويابة سارة","قزريط أسماء","بوسنان سعاد","بوكرموش حفصة","الشيخ صالح فطيمة بنت","حمدي بابا مريم","بوكرموش سيرين","حجاج إسراء","امسيرد ضحى","ليسير بشرى","البدري رحمة","ارفيس فردوس","الشيخ صالح مارية","عوف نسيمة بنت","بازين سلسبيل","بوحديبه خديجة","اويابه بشرى","بابنات صفاء","عطفاوي مامه بنت","باباواسماعيل ملاك","طباخ زينب","قزريط إكرام","موسى وعلي زينب","أوزايد مامة لالة"]
+     ,
+     pickedP: null,
+      currentObj:{
+      number:null,
+      arr: null
+    },
+      currentObjIndex : 0,
+    lists:[{
+      number:"demo",
+      arr: [0,1,2,3,4,5,6]
+    },
+ 
+    
+    ],
+    
+    }
+  },
+   beforeMount() {
+   if(this.currentObj.number === null){ this.currentObj = this.lists[0]}
+  },
+  methods:{
+    listExtrcat(index){
+this.currentObj.number = this.lists[index].number;
+this.currentObj.arr = this.lists[index].arr;
+this.currentObjIndex = index;
+console.log(this.currentObj)
+},
+ studentSelector(){
+
+
+
+  let pickedN = Math.floor(Math.random()*this.currentObj.arr.length);
+  this.pickedP = this.names[this.currentObj.arr[pickedN]]
+  this.currentObj.arr.splice(pickedN,1);
+
+    if( this.currentObj.arr.length === 0){
+      this.currentObj.arr = [...this.arr];
+      console.log(this.arr)
+      
+    }
+
+},
+saver(){
+  this.lists.splice(this.currentObjIndex ,1,this.currentObj);
+  
+},
+newarrcreator(name){
+  this.lists.push({
+    number : name,
+    arr: this.arr
+  });
+
+ this.currentObj = this.lists[this.lists.length-1]
+
+  
+}
+}
+
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1{
+      height: 2rem;
+}
+button{
+  background: #B39CD0;
+  border: 2px solid #B39CD0;
+  color: black;
+  font-size: 1rem;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  border-radius: 3px;
+  margin: 1rem;
+  padding: .5rem;
+  cursor: pointer;
+  min-width: 10vw;
+  min-height: 3vh;
+}
+button:hover{
+  background: none;
+  color: black;
+}
+input{
+  min-width: 50vw;
+  min-height: 10vh;
+  font-size: 3rem;
+  text-align: center;
+}
+.lists-container{
+  display: flex;
+  flex-direction: row;
+}
+
+.saved-list{
+  margin: 0.5rem;
+ min-height: 8rem;
+ min-width:10rem ;
+  border: 1px solid black;
+  padding: 10px;
+    display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;  
+  color: #845EC2;
+  font-size: 3rem;
+}
+</style>
